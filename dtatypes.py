a28e64af5a5ed12f5af6f6a088c3831726fb3db8
abubakarcode-play <abubakarshahidinn786@gmail.com>
2025-06-26 18:48:01 +0500

    Add files via upload

---
+++ dtatypes.py
@@ -0,0 +1,17 @@
+a=5
+print("type of a :", type(a))
+b=2.5
+print("type of b:", type(b))
+c="coding"
+print("type of c:", type(c))
+d=True
+print("type of d:", type(d))
+a=str(a)
+print("type of a is:", type(a))
+print("value of a is:", a)
+b=int(b)
+print("type of a is:", type(b))
+print("value of a is:", b)
+text=input("Enter Your Name:")
+rev=text[::-1]
+print("Reversed String is:", rev)
